4d4f11ed798d0d58f586a85b2d4794af1feb61d6
willybernal <willybernal@gmail.com>
2013-12-07 22:09:53 -0500

    updated importJava with all .jar files

--- source/install/importJava.m
+++ source/install/importJava.m
@@ -7,14 +7,14 @@ import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.Session;
 
 % Import AWS
-import com.amazonaws.AmazonServiceException;
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.PropertiesCredentials;
-import com.amazonaws.services.ec2.AmazonEC2;
-import com.amazonaws.services.ec2.AmazonEC2Client;
-import com.amazonaws.services.ec2.model.*;
-import com.amazonaws.services.s3.*;
-import com.amazonaws.services.simpledb.*;
+% import com.amazonaws.AmazonServiceException;
+% import com.amazonaws.auth.AWSCredentials;
+% import com.amazonaws.auth.PropertiesCredentials;
+% import com.amazonaws.services.ec2.AmazonEC2;
+% import com.amazonaws.services.ec2.AmazonEC2Client;
+% import com.amazonaws.services.ec2.model.*;
+% import com.amazonaws.services.s3.*;
+% import com.amazonaws.services.simpledb.*;
 import com.amazonaws.*;
-import com.amazonaws.services.ec2.AmazonEC2Client;
-import org.apache.http.client.methods.HttpUriRequest;
+% import com.amazonaws.services.ec2.AmazonEC2Client;
+import org.apache.*;%http.client.methods.HttpUriRequest
