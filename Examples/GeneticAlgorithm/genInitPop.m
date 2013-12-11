a8aa5184abd2cc3ce4626af2e2d6e40948faf989
willybernal <willybernal@gmail.com>
2013-12-11 17:39:39 -0500

    Removed previous ga algo version

--- Examples/GeneticAlgorithm/genInitPop.m
+++
@@ -1,8 +0,0 @@
-function pop=genInitPop(popsize,stringlength,dimension)
-%GENINITPOP Generates a random population according to the input parameters
-%
-% 
-
-% Generate Initial Population
-% Contains extra row for fitness
-pop=round(rand(popsize,dimension*stringlength+1));
